fa81d754361eaad75238a1ad060e1cc68bd57bc3
oracle <oracle@localhost.localdomain>
2018-02-20 19:56:34 +0100

    Fin dia 20

--- ej11.py
+++ ej11.py
@@ -7,7 +7,7 @@ print sys.argv[0]+':'
 if len(sys.argv) !=4:
 	print 'Argumento/s incorrecto/s.'
 	exit()
-print 'Iniciales: '+sys.argv[1][0].upper()+sys.argv[2][0].upper()+sys.argv[3][0].upper()+'.'
+print 'Iniciales: '+(sys.argv[1][0]+sys.argv[2][0]+sys.argv[3][0]).upper()+'.'
 
 #Otra forma
 iniciales=sys.argv[1][0]+sys.argv[2][0]+sys.argv[3][0]
---
+++ lib/m1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+def saludar(saludo):
+	print saludo
+
+def iniciales(nom,ap1,ap2):
+	print 'Iniciales: '+(nom[0]+ap1[0]+ap2[0]).upper()+'.'
