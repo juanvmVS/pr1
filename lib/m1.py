3e3cdb2fcca4bd45bbef0861cffc56d37531328b
oracle <oracle@localhost.localdomain>
2018-02-21 15:44:23 +0100

    Hasta funciones

--- lib/m1.py
+++ lib/m1.py
@@ -6,4 +6,12 @@ def saludar(saludo):
 	print saludo
 
 def iniciales(nom,ap1,ap2):
-	print 'Iniciales: '+(nom[0]+ap1[0]+ap2[0]).upper()+'.'
+	return 'Iniciales: '+(nom[0]+ap1[0]+ap2[0]).upper()+'.'
+
+def siglas(nom,*apellidos):
+	if len(apellidos)<1:
+		return 'Falta al menos un apellido.'
+	inic=nom[0]
+	for apellido in apellidos:
+		inic=inic+apellido[0]
+	return inic.upper()
